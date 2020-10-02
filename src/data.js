ccebb6dd34d63bd814ed282033842a703c41ed1c
Drzaln <drzaln@null.net>
2020-10-02 22:02:20 +0700

    first commit

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 
 // use require('./assets/filename') for offline image
+// add image file to assets/images
 // use {uri: 'image url'} for hosted image
 // please use high quality image
 // you are free to add more than one social media or portfolio link
