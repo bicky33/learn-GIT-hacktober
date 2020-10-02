55038b752e8b4ac056c3f8872e0550cbf221bdbb
Drzaln <drzaln@null.net>
2020-10-02 22:12:46 +0700

    edit data.js comment

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
-
-// use require('./assets/filename') for offline image
-// add image file to assets/images
-// use {uri: 'image url'} for hosted image
-// please use high quality image
-// you are free to add more than one social media or portfolio link
+/**
+ * Add your data in next index with same key but different values
+ * use require('./assets/filename') for offline image
+ * add image file to assets/images
+ * use {uri: 'image url'} for hosted image
+ * please use high quality image
+ * you are free to add more than one social media or portfolio link
+**/
 
 export const DATA = [
     {
